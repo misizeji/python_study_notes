f4775222292964f27fd4dc355d31aba7b3c618e4
frodo.ji <frodo.ji@zkteco.com>
2017-07-13 11:01:13 +0800

    modified some words, also add new line to show diff logic.

--- small_game/GuessNumber.py
+++ small_game/GuessNumber.py
@@ -2,6 +2,7 @@ print("input anyone number that you want")
 temp = input("input: ")
 number = int(temp)
 if 6 == number:
-    print("congratulations ! you are right")
+    print("congratulations, you are right!")
 else:
     print("you wrong, game over")
+print("i am tried, bye-bye !")
