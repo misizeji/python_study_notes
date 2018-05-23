bd2210c6c81f634262fe71d472bf8713190efbf4
JITAO <jitao90s@gmail.com>
2018-05-23 15:39:27 +0800

    for pass keyword study

---
+++ pass-sample.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for letter in "Runoob":
+    if letter == 'o':
+        pass
+        print("For command pass keyword")
+    print("now letter: ", letter)
+
+print("Good Bye", end = "!!!!!!")
