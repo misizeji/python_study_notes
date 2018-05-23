88078c3716621eb24bbf76c5cdf42cf18772e41e
JITAO <jitao90s@gmail.com>
2018-05-23 15:09:03 +0800

    for range keyword study

--- range-sample2.py
+++ range-sample2.py
@@ -3,3 +3,17 @@
 a = ["Google", "Baidu", "Runoob", "Taobao", "QQ"]
 for i in range(len(a)):
     print(i, a[i])
+
+for letter in 'Runoob':
+    if letter == 'b':
+        break
+    print('now letter: ', letter)
+
+var = 10
+while var > 0:
+    print('now var: ', var)
+    var = var - 1
+    if var == 5:
+        break
+
+print("Good Bye", end="!!!!!")
