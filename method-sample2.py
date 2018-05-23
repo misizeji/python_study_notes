8bcbfe501b7d0f6d00d92c9f9cd23c6b6d8bbbd3
JITAO <jitao90s@gmail.com>
2018-05-23 17:57:42 +0800

    method study

---
+++ method-sample2.py
@@ -0,0 +1,14 @@
+#！/usr/bin/python3
+
+def printme(str):
+    print(str)
+    print("")
+    return
+
+printme( str = "New bird Lesson")
+
+def printinfo(name, age):
+    print("name: ", name)
+    print("age: ", age)
+    return
+printinfo(age = 50, name = "Runoob")
