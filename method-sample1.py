21203520e7b86ea14afa4ad3d3abc4e776078eb6
JITAO <jitao90s@gmail.com>
2018-05-23 17:30:01 +0800

    method study

--- method-sample1.py
+++ method-sample1.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-
 def area(width, height):
     return width * height
-
 def welcome(name):
     print("welcome",name)
-
 welcome("Runoob")
-
 w, h = 4, 5
 print("width =", w, "height =", h, "area =",area(w, h))
