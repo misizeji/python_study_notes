5f1ae05f62e074892e7482bf36bf572c01162219
JITAO <jitao90s@gmail.com>
2018-05-25 14:37:29 +0800

    str format study

--- output.py
+++ output.py
@@ -8,7 +8,13 @@ for x in range(1, 11):
 
 # 两个print之间终端输出会有换行
 print(1)
-print(2)
+print('2'.rjust(8))
 
 for x in range(1, 11):
     print('{0:2d} {1:3d} {2:4d}'.format(x, x*x, x*x*x))
+
+print('12'.zfill(5))
+print('-3.14'.zfill(7))
+print('3.14159265359'.zfill(5))
+
+print('{} 网址： "{}!"'.format('cainiaojiaocheng', 'www.runoob.com'))
