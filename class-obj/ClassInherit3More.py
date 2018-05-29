edbe825ae95ebc7db12a6fe2a45352e58317d506
JITAO <jitao90s@gmail.com>
2018-05-29 09:38:34 +0800

    for format file syntax

--- class-obj/ClassInherit3More.py
+++ class-obj/ClassInherit3More.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-class people():
+class People():
     name = ''
     age = 0
     __weight = 0
@@ -15,19 +15,21 @@ class people():
         print("%s says that I am %d years old"%(self.name, self.age))
 
 
-class student(people):
+class Student(People):
     grade = 0
+
     def __init__(self, n, a, w, g):
-        people.__init__(self, n, a, w)
+        People.__init__(self, n, a, w)
         self.grade = g
 
     def speak(self):
         print("%s says that I am %d years old, I am in grade %d"%(self.name, self.age, self.grade))
 
 
-class speaker():
+class Speaker():
     topic = ''
     name = ''
+
     def __init__(self, n, t):
         self.name = n
         self.topic = t
@@ -36,12 +38,13 @@ class speaker():
         print("I am %s, a speech speaker, my topic is %s"%(self.name, self.topic))
 
 
-class sample(speaker, student):
+class Sample(Speaker, Student):
     a = ''
-    def __init__(self, n, a, w, g, t):
-        student.__init__(self, n, a, w, g)
-        speaker.__init__(self, n, t)
 
-test = sample("Tim", 25, 80, 9, "python")
+    def __init__(self, n, a, w, g, t):
+        Student.__init__(self, n, a, w, g)
+        Speaker.__init__(self, n, t)
+
+test = Sample("Tim", 25, 80, 9, "python")
 # 方法同名，方法调用括号内，父类排列从左至右依次查找speak()方法，所以此处的speak()方法为speaker类里面的speak方法
 test.speak()
